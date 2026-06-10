818b7a4d868f25705ade35ff2c651b76b449c02f
Frandy Morel <frandymorel3@gmail.com>
2026-06-09 21:32:15 -0400

    Se agregaron modulos, controlladores y servicios necesarios para comenzar el proceso de autenticacion

--- Backend/vestro/src/app.module.ts
+++ Backend/vestro/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from "@nestjs/common";
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { CompaniesController } from './companies/companies.controller';
+import { CompaniesModule } from './companies/companies.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [UserModule, CompaniesModule],
+  controllers: [UserController, CompaniesController],
+  providers: [UserService],
 })
 export class AppModule {}
