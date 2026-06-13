8b235c45a927709bc6ce62af39fc482bd9d59a6d
Frandy Morel <frandymorel3@gmail.com>
2026-06-13 12:07:47 -0400

    Se agrego el modulo de subscripciones, sus endpoints e interfaces correspondientes

--- Backend/vestro/src/subscription/subscription.module.ts
+++ Backend/vestro/src/subscription/subscription.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SubscriptionService } from './subscription.service';
+import { SubscriptionController } from './subscription.controller';
 import { SubscriptionService } from './subscription.service';
 
 @Module({
-  providers: [SubscriptionService]
+  providers: [SubscriptionService],
+  controllers: [SubscriptionController]
 })
 export class SubscriptionModule {}
